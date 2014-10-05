dfabe07ab9b931895e08c5758bf9348abaa3f120
Nicolas CANIART <nicolas@caniart.net>
2014-10-05 22:40:28 +0200

    Fixed typo in the package name the copyright and license statements.

--- ssh_harness/__init__.py
+++ ssh_harness/__init__.py
@@ -2,19 +2,19 @@
 #
 #  Copyright © 2014, Nicolas CANIART <nicolas@caniart.net>
 #
-#  This file is part of vs-ssh.
+#  This file is part of vcs-ssh.
 #
 #  vcs-ssh is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License version 2 as
 #  published by the Free Software Foundation.
 #
-#  vs-ssh is distributed in the hope that it will be useful,
+#  vcs-ssh is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with vs-ssh.  If not, see <http://www.gnu.org/licenses/>.
+#  along with vcs-ssh.  If not, see <http://www.gnu.org/licenses/>.
 #
 from __future__ import print_function
 from unittest import TestCase, SkipTest
