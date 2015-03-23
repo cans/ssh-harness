877a202ba6d96f0a05344d2c98333f14d1d0b3b1
Test User <test@example.com>
2015-03-23 09:32:56 +0100

    Augmented test coverage on the IOCapture context manager and consequently found and fixed a few bugs.

--- ssh_harness/contexts/iocapture.py
+++ ssh_harness/contexts/iocapture.py
@@ -16,22 +16,22 @@
 #  You should have received a copy of the GNU General Public License
 #  along with vcs-ssh.  If not, see <http://www.gnu.org/licenses/>.
 #
-from io import StringIO, BytesIO
+"""
+The :py:mod:`iocapture` module provides the means to capture IO produced
+by other python modules.
+
+One can capture output either globally or on a per module basis, through
+duck-typing.
+
+Here is an example
+
+   w
+"""
+from io import StringIO as MemoryIO
+from gettext import lgettext as _
 import os
 import sys
 from tempfile import TemporaryFile
-
-
-__all__ = [
-    'Py3',
-    'IOCapture',
-    ]
-
-Py3 = True
-MemoryIO = StringIO
-if (3, 0, 0, ) > sys.version_info:
-    Py3 = False
-    MemoryIO = BytesIO
 
 
 class IOCapture(object):
@@ -43,56 +43,72 @@ class IOCapture(object):
                  module=None,
                  out_attr=None,
                  err_attr=None):
+        """Context Manager to capture either or both stdout and stderr.
+        """
         if module is None:
             module = 'sys'
         if module in sys.modules:
             self._mod = sys.modules[module]
-        self._err_attr = 'stderr' if err_attr is None else err_attr
-        self._out_attr = 'stdout' if out_attr is None else out_attr
+        else:
+            raise RuntimeError(
+                _("Module `{}' is not yet loaded!").format(module))
+
         self._do_stderr = stderr
         self._do_stdout = stdout
 
         if stderr:
-            self._olderr = self.stderr = getattr(self._mod, self._err_attr)
-            self._redirect_stderr(file=file)
+            self._prepare_attrs(err_attr or 'stderr', 'err', file)
+        else:
+            self.stderr = sys.stderr
+
         if stdout:
-            self._oldout = self.stdout = getattr(self._mod, self._out_attr)
-            self._redirect_stdout(file=file)
+            self._prepare_attrs(out_attr or 'stdout', 'out', file)
+        else:
+            self.stdout = sys.stdout
 
     def __enter__(self):
         if self._do_stderr:
-            setattr(self._mod, self._err_attr, self.stderr)
+            # Redirect stderr
+            setattr(self._err_target, self._err_attr, self.stderr)
         if self._do_stdout:
-            setattr(self._mod, self._out_attr, self.stdout)
+            # Redirect stdout
+            setattr(self._out_target, self._out_attr, self.stdout)
         return self
 
-    def __exit__(self, exc, exc_type, tb):
-        if exc is None:
-            if self._do_stderr:
-                setattr(self._mod, self._err_attr, self._olderr)
-            if self._do_stdout:
-                setattr(self._mod, self._out_attr, self._oldout)
-            return True
-        else:
-            raise
+    def __exit__(self, exc_type, exc, tb):
+        if self._do_stderr:  # Restore stderr if need be
+            setattr(self._err_target, self._err_attr, self._olderr)
+        if self._do_stdout:  # Restore stdout if need be
+            setattr(self._out_target, self._out_attr, self._oldout)
+        # Whatever happens propagate exceptions.
+        return False
 
-    def _redirect_output(self, name, file=False):
+    def _prepare_attrs(self, module_attr, what, file):
+        parent_object = None
+        value = self._mod
+        for bit in module_attr.split('.'):
+            parent_object = value
+            value = getattr(value, bit)
+
         if file is True:
-            f = TemporaryFile(mode='w+b', prefix='iocpt-')
+            f = TemporaryFile(mode='w+', prefix='iocpt-')
         else:
             f = MemoryIO()
-        setattr(self, name, f)
 
-    def _redirect_stderr(self, file=False):
-        self._redirect_output(self._err_attr, file=file)
-
-    def _redirect_stdout(self, file=False):
-        self._redirect_output(self._out_attr, file=file)
+        setattr(self, '_{}_target'.format(what), parent_object)
+        setattr(self, '_{}_attr'.format(what), bit)
+        setattr(self, '_old{}'.format(what), value)
+        setattr(self, 'std{}'.format(what), f)
 
     def _get_output(self, name):
+        if not getattr(self, '_do_{}'.format(name)):
+            return ''
+        what = name[-3:]
         attr = getattr(self, name)
-        if attr == getattr(self._mod, name):
-            raise Exception()
+        if attr == getattr(getattr(self, '_{}_target'.format(what)),
+                           getattr(self, '_{}_attr'.format(what))):
+            raise RuntimeError(_("Calling `get_std{}()' while in the context"
+                                 " is not allowed!").format(what))
 
         if isinstance(attr, MemoryIO):
             return attr.getvalue()
@@ -101,14 +117,10 @@ class IOCapture(object):
             return attr.read()
 
     def get_stderr(self):
-        if not self._do_stderr:
-            return ''
-        return self._get_output(self._err_attr)
+        return self._get_output('stderr')
 
     def get_stdout(self):
-        if not self._do_stderr:
-            return ''
-        return self._get_output(self._out_attr)
+        return self._get_output('stdout')
 
 
 # vim: syntax=python:sws=4:sw=4:et:
---
+++ ssh_harness/tests/test_iocapture.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8-unix; -*-
+#
+#  Copyright © 2014, Nicolas CANIART <nicolas@caniart.net>
+#
+#  This file is part of vcs-ssh.
+#
+#  vcs-ssh is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 2 as
+#  published by the Free Software Foundation.
+#
+#  vcs-ssh is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with vcs-ssh.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import unicode_literals
+import os
+from unittest import TestCase
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+import sys
+
+from mod4tests import (write_to_stderr, write_to_stdout,
+                       write_to_sys_stderr, write_to_sys_stdout, )
+
+from ssh_harness.contexts import IOCapture
+
+
+class IOCaptureTestCase(TestCase):
+
+    if (3, 0, 0) > sys.version_info:
+        MODULE_NAME = 'ssh_harness.tests.mod4tests'
+    else:
+        MODULE_NAME = 'mod4tests'
+
+    def setUp(self):
+        self.args = ('message,', )
+        self.msg1, self.msg2, self.msg3, self.msg4 = [
+            'message{}\n'.format(x) for x in range(1, 5)
+            ]
+
+    def test_no_capture_stderr(self):
+        with patch('sys.stderr') as mockstderr:
+            write_to_sys_stderr(*self.args)
+
+        mockstderr.write.assert_called_once(self.args)
+
+    def test_no_capture_stdout(self):
+        with patch('sys.stdout') as mockstdout:
+            write_to_sys_stdout(*self.args)
+
+        mockstdout.write.assert_called_once(self.args)
+
+    def test_capture_all_stdout(self):
+        with IOCapture(stdout=True) as io:
+            write_to_sys_stdout(self.msg1)
+            sys.stdout.write(self.msg2)
+
+        write_to_stdout(self.msg3)
+        sys.stdout.write(self.msg4)
+
+        self.assertEqual(io.get_stdout(), self.msg1 + self.msg2)
+        # Message "message3" should be seen on the console.
+
+    def test_capture_all_stderr(self):
+        with IOCapture(stdout=False, stderr=True) as io:
+            write_to_sys_stderr(self.msg1)
+            sys.stderr.write(self.msg2)
+
+        write_to_stderr(self.msg3)
+        sys.stderr.write(self.msg4)
+
+        self.assertEqual(io.get_stderr(), self.msg1 + self.msg2)
+        # Message "message3" should be seen on the console.
+
+    def test_capture_stdout_only_in_module_mod4tests(self):
+        with IOCapture(stdout=True,
+                       module=self.MODULE_NAME,
+                       out_attr='stdout') as io:
+            write_to_stdout(self.msg1)
+            sys.stdout.write(self.msg2)
+
+        write_to_stdout(self.msg3)
+        sys.stdout.write(self.msg4)
+
+        self.assertEqual(io.get_stdout(), self.msg1)
+        # Message "message3" should be seen on the console.
+
+    def test_capture_stderr_only_in_module_mod4tests(self):
+        with IOCapture(stdout=False,
+                       stderr=True,
+                       module=self.MODULE_NAME,
+                       err_attr='stderr') as io:
+            write_to_stderr(self.msg1)
+            sys.stderr.write(self.msg2)
+
+        write_to_stderr(self.msg3)
+        sys.stderr.write(self.msg4)
+
+        self.assertEqual(io.get_stderr(), self.msg1)
+        # Message "message3" should be seen on the console.
+
+    def test_capture_fails_if_module_not_loaded(self):
+        with self.assertRaisesRegexp(RuntimeError,
+                                     "Module `.*' is not yet loaded!"):
+            with IOCapture(module='a_module_that_is_expected_to_not_exists'):
+                pass
+
+    def test_capture_to_file(self):
+        """Ensures redirection to file creates a file indeed.
+
+        .. warning::
+
+           This test may not be portable (see :class:`tempfile.TemporaryFile`)
+           As a matter of fact it is not protable on Linux across Python
+           versions.
+        """
+        filename = None
+        with IOCapture(module=self.MODULE_NAME, stdout=True, file=True) as io:
+            write_to_stdout(self.msg1)
+            filename = io.stdout.name
+            self.assertTrue(os.path.exists(filename)
+                            or (filename.startswith('<')
+                                and filename.endswith('>')))
+
+        self.assertEqual(io.get_stdout(), self.msg1)
+
+    def test_get_output_of_uncaptured_io_returns_empty_string(self):
+        with IOCapture(stderr=False, stdout=False) as io:
+            pass
+
+        self.assertEqual(io.get_stderr(), '')
+        self.assertEqual(io.get_stdout(), '')
+
+    def test_call_to_get_stdout_are_forbidden_within_the_context(self):
+        """Ensure calls to the :method:`iocapture.IOCapture.get_stdout` method
+        are not allowed while *in* the context.
+
+        .. warning::
+
+           This test may not be portable (see :class:`tempfile.TemporaryFile`)
+        """
+        with self.assertRaisesRegexp(
+                RuntimeError,
+                "Calling `.*' while in the context is not allowed!"):
+            with IOCapture(stderr=False,
+                           stdout=True,
+                           module=self.MODULE_NAME) as io:
+                io.get_stdout()
+
+    def test_call_to_get_stderr_are_forbidden_within_the_context(self):
+        """Ensure calls to the :method:`iocapture.IOCapture.get_stderr` method
+        are not allowed while *in* the context.
+
+        .. warning::
+
+           This test may not be portable (see :class:`tempfile.TemporaryFile`)
+        """
+        with self.assertRaisesRegexp(
+                RuntimeError,
+                "Calling `.*' while in the context is not allowed!"):
+            with IOCapture(stderr=True,
+                           stdout=False,
+                           module=self.MODULE_NAME) as io:
+                io.get_stderr()
+
+
+# vim: syntax=python:sws=4:sw=4:et:
